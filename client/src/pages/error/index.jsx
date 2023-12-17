b2f3e4a78da90bbc1780fa332bc2eab800c54841
ELHart05 <o.allaoua@esi-sba.dz>
2023-12-17 22:40:53 +0100

    fixed error page

---
+++ client/src/pages/error/index.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout';
+import './style.css';
+
+const Error = () => {
+    return (
+        <Layout>
+            This is a error page
+        </Layout>
+    )
+}
+
+export default Error
