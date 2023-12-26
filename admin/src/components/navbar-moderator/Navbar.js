c44af24a44d0360eb7630dd254fcd55853c6c87c
bouchrabenkhelifa <benbouchra314@gmail.com>
2023-12-26 10:11:34 +0300

    moderator layout

---
+++ admin/src/components/navbar-moderator/Navbar.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import "./navbar.css";
+const Navbar = () => {
+    return(
+        <div className='allll'>
+<div className='topp'> 
+            <h2> PaperHub</h2> 
+             <button id="btnnn" > log out  </button> 
+             <div className='line'></div>
+        </div>
+        <div className='middlle'> 
+            <h2> Articles</h2> 
+             <input id="searchh" type="text" placeholder="Search articles "/>    
+        </div>
+        </div>
+    );}
+
+export default Navbar
