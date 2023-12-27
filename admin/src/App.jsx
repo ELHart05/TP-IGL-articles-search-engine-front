a9cf2fbf58184687ae02fb0123362ebf74f096f9
okbaallaoua <o.a@swissdigilab.com>
2023-12-27 23:41:24 +0100

    fixed admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,4 @@
 import { Route, Routes } from "react-router-dom"
-import PrivateRoute from "./middleware/PrivateRoute"
-import Home from "./pages/home"
-import About from "./pages/about"
 import Error from "./pages/error"
 import Add_moderator from "./pages/add_moderator/Add";
 import Admin from "./pages/modify_admin/Addadmin";
@@ -12,16 +9,7 @@ import GererModerator from "./pages/gerer_moderator"
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/about"
-          element={
-            <PrivateRoute>
-              <About />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/gerer-article" element={<GererArticle />} />
+        <Route path="/" element={<GererArticle />} />
         <Route path="/gerer-moderator" element={<GererModerator />} />
         <Route path="/add-moderator" element={<Add_moderator />} />
         <Route path="/admin-info" element={<Admin />} />
