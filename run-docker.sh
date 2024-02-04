b5da2d850cbe2cf0ffb2d9b316b7fec5d85533a2
ELHart05 <o.allaoua@esi-sba.dz>
2024-02-04 07:21:33 +0100

    added docker compose

--- run-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t papershub-image .
-
-# Run the Docker container for papersHub, exposing the necessary ports
-docker run -p 8081:3001 -p 8080:3000 papershub-image
